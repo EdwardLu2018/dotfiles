74b55da1ddbf0f0e3c5b4599d22aba96e2e1d5e4
Edward <lued2018@gmail.com>
2022-03-11 20:58:29 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install Packages
+git clone https://github.com/Raimondi/delimitMate ~/.vim/bundle/delimitMate
+git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommentor
+git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
