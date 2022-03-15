ef78cb772ee41142579ab7cb615d60fdecb9eba7
Edward <lued2018@gmail.com>
2022-03-15 15:50:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,4 +14,4 @@ git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommentor
 git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-
+git clone https://github.com/ackyshake/VimCompletesMe ~/.vim/bundle/VimCompletesMe
