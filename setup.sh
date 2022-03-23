e193090f4be007996f36ee10ecade8eafebf1350
Edward <lued2018@gmail.com>
2022-03-23 13:16:57 -0400

    add syntax highlighter

--- setup.sh
+++ setup.sh
@@ -15,3 +15,4 @@ git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/ackyshake/VimCompletesMe ~/.vim/bundle/VimCompletesMe
+git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/bundle/vim-polyglot
