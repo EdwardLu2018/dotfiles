f756ed3ca502aa5a0ac4530fffba9f3351c155e8
Edward <lued2018@gmail.com>
2022-04-06 23:25:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 # Copy dotfiles
 cp .vimrc ~/.vimrc
-mkdir ~/.vim/colors/
-cp predawn.vim ~/.vim/colors/predawn.vim
 cp .tmux.conf ~/.tmux.conf
 
 # Install Pathogen
@@ -16,3 +14,7 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/ackyshake/VimCompletesMe ~/.vim/bundle/VimCompletesMe
 git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/bundle/vim-polyglot
+
+# Copy theme
+[ ! -d "~/.vim/colors/" ] mkdir ~/.vim/colors/
+cp predawn.vim ~/.vim/colors/predawn.vim
