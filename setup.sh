413305903233d616b18065db975ff004d42bec8a
Edward Lu <lued2018@gmail.com>
2023-05-13 12:44:58 -0400

    fix broken github link

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommentor
 git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone https://github.com/ackyshake/VimCompletesMe ~/.vim/bundle/VimCompletesMe
+git clone https://github.com/vim-scripts/VimCompletesMe ~/.vim/bundle/VimCompletesMe
 git clone https://github.com/tmhedberg/matchit ~/.vim/bundle/matchit
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/bundle/vim-polyglot
 git clone https://github.com/junegunn/fzf ~/.vim/bundle/fzf
